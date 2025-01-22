60525f46986087acb3659944f08703f234aac57a
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2025-01-22 21:44:57 +0500

    create the author controller, modal and update petition modal

--- app/Http/Controllers/API/V1/AuthorController.php
+++ app/Http/Controllers/API/V1/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 
 use App\Models\Author;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AuthorResource;
 use App\Http\Resources\AuthorCollection;
