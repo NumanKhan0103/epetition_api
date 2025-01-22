15d5a6f66abcf25e94293933ec9407a87ec7a005
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2025-01-22 15:35:51 +0500

    create modal , controller and  resource collection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-    })no
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
